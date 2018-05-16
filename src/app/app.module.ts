733adc79caabdc00809fba2772c525d4a0e98b5e
Daniele Maraschi <d.maraschi@gmail.com>
2018-05-16 15:14:29 +0200

    Initial rel

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppmenuComponent } from './components/appmenu/appmenu.component';
+import { AppsettingComponent } from './components/appsetting/appsetting.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppheaderComponent,
+    AppfooterComponent,
+    AppmenuComponent,
+    AppsettingComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
